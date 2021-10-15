c904712cdbcf23daf1878f66e39c79b053e558be
Edonis Seferi <edonis.seferi@asos.com>
2021-10-15 21:01:53 +0100

    project files added

---
+++ src/RoadStatus.Console/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RoadStatus.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/RoadStatus.Service/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace RoadStatus.Service
+{
+    public class Class1
+    {
+    }
+}
