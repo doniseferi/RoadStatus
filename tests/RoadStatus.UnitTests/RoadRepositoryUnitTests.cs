0272c9addf87dd8fde5657a8020e4e44c0ccf803
Edonis Seferi <edonis.seferi@asos.com>
2021-10-18 20:17:15 +0100

    faling test for found road added

--- tests/RoadStatus.UnitTests/RoadRepositoryUnitTests.cs
+++ tests/RoadStatus.UnitTests/RoadRepositoryUnitTests.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Flurl.Http;
 using Flurl.Http.Testing;
 using LanguageExt;
+using Newtonsoft.Json;
 using NUnit.Framework;
 using RoadStatus.Data;
 using RoadStatus.Service.Entities;
@@ -13,7 +14,6 @@ namespace RoadStatus.UnitTests
     [TestFixture]
     internal class RoadRepositoryUnitTests
     {
-
         [Test]
         public async Task Throws_An_Exception_When_Null_Is_Passed_In()
         {
@@ -28,10 +28,35 @@ namespace RoadStatus.UnitTests
         {
             using var httpTest = new HttpTest();
             httpTest.RespondWith(status: 404);
-            
+
             var roadRepository = new RoadRepository(new FlurlClient());
 
             var expected = Option<Road>.None;
+            var actual = await roadRepository.GetByIdAsync(new RoadId(roadId));
+
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        [Test]
+        [TestCase("western cross route")]
+        [TestCase("city route")]
+        public async Task Returns_A_Road_When_A_Valid_Road_Id_Is_Provided(string roadId)
+        {
+            var expectedRoad = new Road(
+                new RoadId(roadId),
+                new Name(roadId),
+                new Status(
+                    new Severity("Good"),
+                    new Description("No Exceptional Delays")));
+
+            var mockedResult = JsonConvert.SerializeObject(expectedRoad);
+
+            using var httpTest = new HttpTest();
+            httpTest.RespondWith(status: 200, body: mockedResult);
+
+            var roadRepository = new RoadRepository(new FlurlClient());
+
+            var expected = Option<Road>.Some(expectedRoad);
             var actual = await roadRepository.GetByIdAsync(new RoadId(roadId));
 
             Assert.That(expected, Is.EqualTo(actual));
