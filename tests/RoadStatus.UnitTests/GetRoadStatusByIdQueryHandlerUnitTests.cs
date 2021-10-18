778404748c775efdca5c4a90d69f41456466a9f3
Edonis Seferi <edonis.seferi@asos.com>
2021-10-18 10:15:38 +0100

    failing handler tests added

--- tests/RoadStatus.UnitTests/GetRoadStatusByIdQueryHandlerUnitTests.cs
+++ tests/RoadStatus.UnitTests/GetRoadStatusByIdQueryHandlerUnitTests.cs
@@ -1,5 +1,5 @@
 ﻿using System.Threading.Tasks;
-using Microsoft.VisualStudio.TestPlatform.Common.Utilities;
+using LanguageExt;
 using Moq;
 using NUnit.Framework;
 using RoadStatus.Service.Entities;
@@ -15,21 +15,92 @@ namespace RoadStatus.UnitTests
         [TestCase("a1", "A1")]
         [TestCase("a406", "North Circular(A406)")]
         [TestCase("a404", "A404(M)")]
-        public async Task Returns_The_Road_Name_When_A_Valid_Road_Id_Is_Found(string roadId, string expectedName)
+        public async Task Returns_The_Road_Name_When_A_Valid_Road_Id_Is_Provided(string roadId, string expectedName)
         {
             var mockedRepo = new Mock<IRoadRepository>();
-            mockedRepo.Setup(x => x.GetByIdAsync(It.Is<RoadId>(id => id.Value == roadId))).ReturnsAsync(
-                new Road(new RoadId(roadId), new Name(expectedName),
-                    new Status(new Severity("Mild"), new Description("Good"))));
+            mockedRepo.Setup(
+                    x => x.GetByIdAsync(It.Is<RoadId>(id => id.Value == roadId)))
+                .ReturnsAsync(
+                    new Road(new RoadId(roadId), new Name(expectedName),
+                        new Status(new Severity("Mild"), new Description("Good"))));
 
             var handler = new GetRoadStatusByIdQueryHandler(mockedRepo.Object);
-            var result = await handler.HandleAsync(new RoadId("A1"));
+            var result = await handler.HandleAsync(new RoadId(roadId));
             var actual = result
                 .Map(c => c.Name)
                 .Map(x => x.Value)
                 .IfNone(string.Empty);
 
-            Assert.That(actual, Is.EqualTo(actual));
+            Assert.That(actual, Is.EqualTo(expectedName));
+        }
+
+        [Test]
+        [TestCase("a1", "Good")]
+        [TestCase("a406", "Closure")]
+        public async Task Returns_The_Road_Status_When_A_Valid_Road_Id_Is_Provided(string roadId,
+            string expectedDescription)
+        {
+            var mockedRepo = new Mock<IRoadRepository>();
+            mockedRepo.Setup(
+                    x => x.GetByIdAsync(It.Is<RoadId>(id => id.Value == roadId)))
+                .ReturnsAsync(
+                    new Road(new RoadId(roadId), new Name(roadId),
+                        new Status(new Severity(expectedDescription), new Description("Abc"))));
+
+            var handler = new GetRoadStatusByIdQueryHandler(mockedRepo.Object);
+            var result = await handler.HandleAsync(new RoadId(roadId));
+            var actual = result
+                .Map(c => c.Status)
+                .Map(x => x.Severity)
+                .Map(x => x.Value)
+                .IfNone(string.Empty);
+
+            Assert.That(actual, Is.EqualTo(expectedDescription));
+        }
+
+        [Test]
+        [TestCase("a4", "Closure")]
+        [TestCase("a406", "No Exceptional Delays")]
+        public async Task Returns_The_Road_Status_Description_When_A_Valid_Road_Id_Is_Provided(string roadId,
+            string expectedDescription)
+        {
+            var mockedRepo = new Mock<IRoadRepository>();
+            mockedRepo.Setup(
+                    x => x.GetByIdAsync(It.Is<RoadId>(id => id.Value == roadId)))
+                .ReturnsAsync(
+                    new Road(new RoadId(roadId), new Name(roadId),
+                        new Status(new Severity(expectedDescription), new Description("Abc"))));
+
+            var handler = new GetRoadStatusByIdQueryHandler(mockedRepo.Object);
+            var result = await handler.HandleAsync(new RoadId(roadId));
+            var actual = result
+                .Map(c => c.Status)
+                .Map(x => x.Description)
+                .Map(x => x.Value)
+                .IfNone(string.Empty);
+
+            Assert.That(actual, Is.EqualTo(expectedDescription));
+        }
+
+        [Test]
+        [TestCase("tfl123")]
+        [TestCase("abc123")]
+        public async Task Returns_None_When_The_Road_Is_Not_Found(string roadId)
+        {
+            var mockedRepo = new Mock<IRoadRepository>();
+            mockedRepo.Setup(x => x.GetByIdAsync(It.IsAny<RoadId>()))
+                .ReturnsAsync(
+                    Option<Road>.None);
+
+            var handler = new GetRoadStatusByIdQueryHandler(mockedRepo.Object);
+            var result = await handler.HandleAsync(new RoadId(roadId));
+            var actual = result
+                .Map(c => c.Status)
+                .Map(x => x.Description)
+                .Map(x => x.Value)
+                .IfNone(string.Empty);
+
+            Assert.That(actual, Is.EqualTo(Option<Road>.None));
         }
     }
 }
